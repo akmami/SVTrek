f6c167f3c1b5e5d38368433c8fb478c207d3a91f
Akmuhammet Ashyralyyev <akmuhammet@donut.cs.bilkent.edu.tr>
2022-10-12 00:33:53 +0300

    Program completed

--- sveldt.cpp
+++ sveldt.cpp
@@ -8,7 +8,13 @@
 #include <fstream>
 #include <boost/algorithm/string.hpp>
 #include <vector>
+#include <cmath>
 #include "argh.h"
+
+#define WIDER_INTERVAL          40000
+#define NARROW_INTERVAL         2000
+#define CONSENSUS_INTEVAL       5
+#define CONSENSUS_COUNT_PERC    0.4
 
 using namespace std;
 
@@ -23,6 +29,10 @@ bool process_line(string line);
 bool insertion(string id, int chrom, int outer_start, int inner_start, int inner_end, int outer_end);
 bool deletion(string id, int chrom, int outer_start, int inner_start, int inner_end, int outer_end);
 bool inversion(string id, int chrom, int outer_start, int inner_start, int inner_end, int outer_end);
+int find_start(int chrom, int outer_start, int inner_start);
+int find_end(int chrom, int inner_end, int outer_end);
+int find_start_or_end(int chrom, int start, int end);
+int consensus(vector<int> locations);
 
 // MARK: - global variables
 
@@ -49,10 +59,7 @@ int main(int argc, char *argv[]){
         cout << "VCF file is missing." << endl;
         exit(-1);
     }
-    cout << "OUTPUT" << endl;
-    if ( !( cmdl({"--output", "-o"}) >> output_str ) ) {
-        // output = vcf + ".output";
-    }
+    
     
     const char* bam = bam_str.c_str();
     const char* vcf = vcf_str.c_str();
@@ -111,7 +118,7 @@ bool process_line(string line) {
     if (line.find("IMPRECISE") == string::npos || line.find("SVLEN") != string::npos || line.find("CIPOS") != string::npos) {
         
         // local variables
-        int comp, chrom, sv_pos;
+        int comp, chrom, sv_pos, sv_end;
         string id, alt;
         
         vector<string> splitted_line;
@@ -130,7 +137,7 @@ bool process_line(string line) {
                            ( splitted_line[0].length() == 4 && (splitted_line[0][3] == 'Y' || splitted_line[0][3] == 'y') ) ){
                     // chrom is Y
                 } else {
-                    cout << "Chromosome could not be cinverted into integer. CHROM tag : " << splitted_line[0] << endl;
+                    cout << "# Chromosome could not be converted into integer. CHROM tag : " << splitted_line[0] << endl;
                 }
                 return false;
             }
@@ -156,6 +163,24 @@ bool process_line(string line) {
             return false;
         }
         
+        // Retrieve END
+        int sv_end_start = splitted_line[7].find("END=");
+        int sv_end_end = splitted_line[7].find(";", sv_end_start+1);
+        
+        if (sv_end_start == -1) {
+            return false;
+        }
+        if (sv_end_end == -1) {
+            sv_end_end = splitted_line[7].length();
+        }
+        
+        try {
+            sv_end = stoi( splitted_line[7].substr(sv_end_start+4, sv_end_end) );
+        } catch(...) {
+            cout << "# Unable to convert into int" << splitted_line[7].substr(sv_end_start+4, sv_end_end) << endl;
+        }
+        
+        
         // Retrieve CIPOS and CIEND index
         // CIPOS
         int CIPOS_start = splitted_line[7].find("CIPOS=");
@@ -168,13 +193,13 @@ bool process_line(string line) {
         try {
             outer_start = stoi( splitted_line[7].substr(CIPOS_start+6, CIPOS_comma) );
         } catch(...) {
-            cout << "Unable to convert into int" << splitted_line[7].substr(CIPOS_start+6, CIPOS_comma) << endl;
+            cout << "# Unable to convert into int" << splitted_line[7].substr(CIPOS_start+6, CIPOS_comma) << endl;
         }
         
         try {
             inner_start = stoi( splitted_line[7].substr(CIPOS_comma+1, CIPOS_end) );
         } catch(...) {
-            cout << "Unable to convert into int" << splitted_line[7].substr(CIPOS_comma+1, CIPOS_end) << endl;
+            cout << "# Unable to convert into int" << splitted_line[7].substr(CIPOS_comma+1, CIPOS_end) << endl;
         }
         
         // CIPOS
@@ -188,22 +213,22 @@ bool process_line(string line) {
         try {
             inner_end = stoi( splitted_line[7].substr(CIEND_start+6, CIEND_comma) );
         } catch(...) {
-            cout << "Unable to convert into int" << splitted_line[7].substr(CIEND_start+6, CIEND_comma) << endl;
+            cout << "# Unable to convert into int" << splitted_line[7].substr(CIEND_start+6, CIEND_comma) << endl;
         }
         
         try {
             outer_end = stoi( splitted_line[7].substr(CIEND_comma+1, CIEND_end) );
         } catch(...) {
-            cout << "Unable to convert into int" << splitted_line[7].substr(CIEND_comma+1, CIEND_end) << endl;
+            cout << "# Unable to convert into int" << splitted_line[7].substr(CIEND_comma+1, CIEND_end) << endl;
         }
         
         // Call relevant function to refine SV
         if ( splitted_line[7].find("INS", type_index + 7) != -1 || splitted_line[7].find("ins", type_index + 7) != -1 ) {
-            insertion(id, chrom, outer_start, inner_start, inner_end, outer_end);
+            insertion(id, chrom, sv_pos+outer_start, sv_pos+inner_start, sv_end+inner_end, sv_end+outer_end);
         } else if ( splitted_line[7].find("DEL", type_index + 7) != -1 || splitted_line[7].find("del", type_index + 7) != -1 ) {
-            deletion(id, chrom, outer_start, inner_start, inner_end, outer_end);
+            deletion(id, chrom, sv_pos+outer_start, sv_pos+inner_start, sv_end+inner_end, sv_end+outer_end);
         } else if ( splitted_line[7].find("INV", type_index + 7) != -1 || splitted_line[7].find("inv", type_index + 7) != -1 ) {
-            inversion(id, chrom, outer_start, inner_start, inner_end, outer_end);
+            inversion(id, chrom, sv_pos+outer_start, sv_pos+inner_start, sv_end+inner_end, sv_end+outer_end);
         } else {
             // Other type of sv
             return false;
@@ -213,8 +238,8 @@ bool process_line(string line) {
     return true;
 }
 
-bool insertion(char *id, int chrom, int outer_start, int inner_start, int inner_end, int outer_end) {
-
+int find_start(int chrom, int outer_start, int inner_start) {
+    
     // sam_read1 variables
     int32_t pos;
     char *chr;
@@ -222,9 +247,8 @@ bool insertion(char *id, int chrom, int outer_start, int inner_start, int inner_
     uint32_t flag;
     
     hts_itr_t *iter;
-    iter = sam_itr_queryi( bam_file_index, chrom, outer_start - 40000, inner_start + 2000);
+    iter = sam_itr_queryi( bam_file_index, chrom, outer_start - WIDER_INTERVAL, inner_start + NARROW_INTERVAL);
     vector<int> start_positions;
-    vector<int> end_positions;
     
     if (iter == NULL) {
         printf("# invalid interval, iter is null\n");
@@ -249,7 +273,23 @@ bool insertion(char *id, int chrom, int outer_start, int inner_start, int inner_
     }
     sam_itr_destroy(iter);
     
-    iter = sam_itr_queryi( bam_file_index, chrom, inner_end - 2000, outer_end + 2000);
+    sort(start_positions.begin(), start_positions.end());
+    
+    return consensus(start_positions);
+}
+
+int find_end(int chrom, int inner_end, int outer_end) {
+    
+    // sam_read1 variables
+    int32_t pos;
+    char *chr;
+    size_t read_len;
+    uint32_t flag;
+    
+    hts_itr_t *iter;
+    vector<int> end_positions;
+    iter = sam_itr_queryi( bam_file_index, chrom, inner_end - NARROW_INTERVAL, outer_end + NARROW_INTERVAL);
+    
     if (iter == NULL) {
         printf("# invalid interval, iter is null\n");
     } else {
@@ -266,15 +306,13 @@ bool insertion(char *id, int chrom, int outer_start, int inner_start, int inner_
     
     sam_itr_destroy(iter);
     
-    // Pre-process positions and take consensus
-    // TO-DO
+    sort(end_positions.begin(), end_positions.end());
     
-    return true;
+    return consensus(end_positions);
 }
 
-
-bool deletion(char *id, int chrom, int outer_start, int inner_start, int inner_end, int outer_end) {
-
+int find_start_or_end(int chrom, int start, int end) {
+    
     // sam_read1 variables
     int32_t pos;
     char *chr;
@@ -282,9 +320,8 @@ bool deletion(char *id, int chrom, int outer_start, int inner_start, int inner_e
     uint32_t flag;
     
     hts_itr_t *iter;
-    iter = sam_itr_queryi( bam_file_index, chrom, outer_start - 40000, inner_start + 2000);
-    vector<int> start_positions;
-    vector<int> end_positions;
+    iter = sam_itr_queryi( bam_file_index, chrom - 1, start - WIDER_INTERVAL, end + NARROW_INTERVAL);
+    vector<int> positions;
     
     if (iter == NULL) {
         printf("# invalid interval, iter is null\n");
@@ -292,7 +329,8 @@ bool deletion(char *id, int chrom, int outer_start, int inner_start, int inner_e
         while (sam_itr_next( fp_in, iter, aln ) > 0) {
             pos = aln->core.pos;
             uint32_t *cigar = bam_get_cigar(aln);
-
+            
+            // If read ends in between (start, end)
             if ( bam_cigar_op( cigar[aln->core.n_cigar-1] ) == 4) {
                 int reference_pos = pos;
                 for ( int i = 0; i < aln->core.n_cigar; i++ ) {
@@ -300,109 +338,75 @@ bool deletion(char *id, int chrom, int outer_start, int inner_start, int inner_e
                         reference_pos += bam_cigar_oplen( cigar[i] );
                     }
                 }
-                if ( outer_start <= reference_pos && reference_pos <= inner_start ) {
-                    start_positions.push_back(reference_pos+1);
+                if ( start <= reference_pos && reference_pos <= end ) {
+                    positions.push_back(reference_pos+1);
                 }
             }
-        }
-    }
-    sam_itr_destroy(iter);
-    
-    iter = sam_itr_queryi( bam_file_index, chrom, inner_end - 2000, outer_end + 2000);
-    if (iter == NULL) {
-        printf("# invalid interval, iter is null\n");
-    } else {
-        while (sam_itr_next( fp_in, iter, aln ) > 0) {
-            pos = aln->core.pos;
-            uint32_t *cigar = bam_get_cigar(aln);
 
-            // Even if the read starts with soft clip, the pos is matched with aligned part
-            // Hence, some reads can be without soft clip and directly matched with the reference genome.
-            if ( bam_cigar_op( cigar[0] ) == 4 && inner_end <= pos && pos <= outer_end ) {
-                end_positions.push_back(pos+1);
+            // If read starts in between (start, end)
+            if ( bam_cigar_op( cigar[0] ) == 4 && start <= pos && pos <= end ) {
+                positions.push_back(pos);
             }
         }
     }
     sam_itr_destroy(iter);
     
-    // Pre-process positions and take consensus
-    // TO-DO
+    sort(positions.begin(), positions.end());
+    
+    return consensus(positions);
+}
+
+int consensus(vector<int> locations) {
+    int length = locations.size(), i;
+    int consensus = -1, max_count = - 1;
+    vector<int>::iterator ptr_i;
+    vector<int>::iterator ptr_j;
+    
+    for (ptr_i = locations.begin(), i = 0; ptr_i<locations.end(); ptr_i++, i++) {
+        int item_i = *ptr_i;
+        int sum = 0, count = 1;
+        for (ptr_j = locations.begin()+i; ptr_j<locations.end() && *ptr_j<*ptr_i+CONSENSUS_INTEVAL; ptr_j++) {
+            sum += (*ptr_j) - (*ptr_i);
+            count++;
+        }
+        
+        sum = (*ptr_i) + round(sum / count);
+        
+        if (count > CONSENSUS_COUNT_PERC * length && count > max_count) {
+            max_count = count;
+            consensus = sum;
+        }
+    }
+    
+    return consensus;
+}
+
+bool insertion(string id, int chrom, int outer_start, int inner_start, int inner_end, int outer_end) {
+
+    int refined_start = find_start(chrom, outer_start, inner_start);
+    int refined_end = find_end(chrom, inner_end, outer_end);
+    
+    cout << id << "\t" << "ins\t" << chrom << "\t" << refined_start << "\t" << refined_end << endl;
     
     return true;
 }
 
-bool inversion(char *id, int chrom, int outer_start, int inner_start, int inner_end, int outer_end) {
+bool deletion(string id, int chrom, int outer_start, int inner_start, int inner_end, int outer_end) {
+    
+    int refined_pos = find_start_or_end(chrom, outer_start, inner_start);
+    
+    cout << id << "\t" << "del\t" << chrom << "\t" << refined_pos << endl;
+    
+    return true;
+}
+
+bool inversion(string id, int chrom, int outer_start, int inner_start, int inner_end, int outer_end) {
 
     // sam_read1 variables
-    int32_t pos;
-    char *chr;
-    size_t read_len;
-    uint32_t flag;
+    int refined_start = find_start_or_end(chrom, outer_start, inner_start);
+    int refined_end = find_start_or_end(chrom, inner_end, outer_end);
     
-    hts_itr_t *iter;
-    iter = sam_itr_queryi( bam_file_index, chrom - 1, outer_start - 40000, inner_start + 2000);
-    vector<int> start_positions;
-    vector<int> end_positions;
-    
-    if (iter == NULL) {
-        printf("# invalid interval, iter is null\n");
-    } else {
-        while (sam_itr_next( fp_in, iter, aln ) > 0) {
-            pos = aln->core.pos;
-            uint32_t *cigar = bam_get_cigar(aln);
-            
-            // If read ends in between (outer_start, inner_start)
-            if ( bam_cigar_op( cigar[aln->core.n_cigar-1] ) == 4) {
-                int reference_pos = pos;
-                for ( int i = 0; i < aln->core.n_cigar; i++ ) {
-                    if ( bam_cigar_op( cigar[i] ) != 1 && bam_cigar_op( cigar[i] ) != 4 && bam_cigar_op( cigar[i] ) != 5 && bam_cigar_op( cigar[i] ) != 6 ) {
-                        reference_pos += bam_cigar_oplen( cigar[i] );
-                    }
-                }
-                if ( outer_start <= reference_pos && reference_pos <= inner_start ) {
-                    start_positions.push_back(reference_pos+1);
-                }
-            }
-
-            // If read starts in between (outer_start, inner_start)
-            if ( bam_cigar_op( cigar[0] ) == 4 && outer_start <= pos && pos <= inner_start ) {
-                start_positions.push_back(pos+1);
-            }
-        }
-    }
-    sam_itr_destroy(iter);
-    
-    iter = sam_itr_queryi( bam_file_index, chrom, inner_end - 40000, outer_end + 2000);
-    if (iter == NULL) {
-        printf("# invalid interval, iter is null\n");
-    } else {
-        while (sam_itr_next( fp_in, iter, aln ) > 0) {
-            pos = aln->core.pos;
-            uint32_t *cigar = bam_get_cigar(aln);
-            
-            // If read ends in between (inner_end, outter_end)
-            if ( bam_cigar_op( cigar[aln->core.n_cigar-1] ) == 4) {
-                int reference_pos = pos;
-                for ( int i = 0; i < aln->core.n_cigar; i++ ) {
-                    if ( bam_cigar_op( cigar[i] ) != 1 && bam_cigar_op( cigar[i] ) != 4 && bam_cigar_op( cigar[i] ) != 5 && bam_cigar_op( cigar[i] ) != 6 ) {
-                        reference_pos += bam_cigar_oplen( cigar[i] );
-                    }
-                }
-                if ( inner_end <= reference_pos && reference_pos <= outer_end ) {
-                    end_positions.push_back(reference_pos+1);
-                }
-            }
-            
-            // If read starts in between (inner_end, outter_end)
-            if ( bam_cigar_op( cigar[0] ) == 4 && inner_end <= pos && pos <= outer_end ) {
-                end_positions.push_back(pos+1);
-            }
-        }
-    }
-    sam_itr_destroy(iter);
-    
-    // Pre-process positions and take consensus
-    // TO-DO
+    cout << id << "\t" << "inv\t" << chrom << "\t" << refined_start << "\t" << refined_end << endl;
     
     return true;
 }
