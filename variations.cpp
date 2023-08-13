fca914ddd2b9d2f15a664237c889e3c3ca1664fb
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2023-08-13 17:35:13 +0300

    algorithm to calc length for INS is implemened, variations within cigar is taken into account

--- variations.cpp
+++ variations.cpp
@@ -4,18 +4,14 @@
 #include "params.h"
 #include "refinement.cpp"
 
-struct result {
-	int refined_start;
-	int refined_end;
-};
 
 namespace sv {
 
 	result deletion(int chrom, int outer_start, int inner_start, int inner_end, int outer_end, int imprecise_pos, int imprecise_end, params &_params) {
 	    
 		result res;
-	    res.refined_start = refine_start(chrom, outer_start, inner_start, imprecise_pos, _params);
-	    res.refined_end = refine_end(chrom, inner_end, outer_end, imprecise_end, _params);
+	    refine_start(chrom, outer_start, inner_start, imprecise_pos, res, _params, DELETION);
+	    refine_end(chrom, inner_end, outer_end, imprecise_end, res, _params, DELETION);
 	    
 	    return res;
 	};
@@ -23,7 +19,7 @@ namespace sv {
 	result insertion(int chrom, int outer_start, int inner_start, int imprecise_pos,  params &_params) {
 
 		result res;
-	    int refined_start = refine_point(chrom, outer_start, inner_start, imprecise_pos, _params);
+	    refine_point(chrom, outer_start, inner_start, imprecise_pos, res, _params, INSERTION);
 
 	    return res;
 	};
@@ -31,8 +27,8 @@ namespace sv {
 	result inversion(int chrom, int outer_start, int inner_start, int inner_end, int outer_end, int imprecise_pos, int imprecise_end, params &_params) {
 
 	    result res;
-	    res.refined_start = refine_point(chrom, outer_start, inner_start, imprecise_pos, _params);
-	    res.refined_end = refine_point(chrom, inner_end, outer_end, imprecise_end, _params);
+	   	refine_point(chrom, outer_start, inner_start, imprecise_pos, res, _params);
+	    refine_point(chrom, inner_end, outer_end, imprecise_end, res, _params);
 
 	    return res;
 	};
