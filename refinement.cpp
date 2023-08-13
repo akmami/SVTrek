a8aeaffd791aaecb4e1b2130d6ba2b6892e649ae
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2023-08-13 17:35:13 +0300

    algorithm to calc length for INS is implemened, variations within cigar is taken into account

--- refinement.cpp
+++ refinement.cpp
@@ -10,11 +10,51 @@
 #include <cmath>
 #include <algorithm>
 
+enum svtype {
+    DELETION,
+    INSERTION,
+    INVERSION,
+};
+
+struct result {
+    int refined_start;
+    int refined_end;
+    int svlen;
+};
+
+
 // MARK: CONSENSUS
 
-int consensus(std::vector<int> locations, int imprecise_pos, params &_params) {
+int consensus(std::vector<int> lengths, params &_params) {
 
-    int length = locations.size(), i;
+    int i;
+    int consensus = -1, max_count = _params.consensus_min_count - 1, distance = INT_MAX;
+    std::vector<int>::iterator ptr_i;
+    std::vector<int>::iterator ptr_j;
+
+    sort(lengths.begin(), lengths.end());
+
+    for (ptr_i = lengths.begin(), i = 0; ptr_i < lengths.end(); ptr_i++, i++) {
+        int sum = 0, count = 1;
+        for (ptr_j = lengths.begin() + i; ptr_j < lengths.end() && *ptr_j <= *ptr_i + _params.consensus_interval; ptr_j++) {
+            sum += (*ptr_j) - (*ptr_i);
+            count++;
+        }
+        
+        sum = (*ptr_i) + round(sum / count);
+        
+        if (count > max_count) {
+            max_count = count;
+            consensus = sum;
+        }
+    }
+    
+    return consensus;
+};
+
+int consensus_pos(std::vector<int> locations, int imprecise_pos, params &_params) {
+
+    int i;
     int consensus = -1, max_count = _params.consensus_min_count - 1, distance = INT_MAX;
     std::vector<int>::iterator ptr_i;
     std::vector<int>::iterator ptr_j;
@@ -22,7 +62,6 @@ int consensus(std::vector<int> locations, int imprecise_pos, params &_params) {
     sort(locations.begin(), locations.end());
 
     for (ptr_i = locations.begin(), i = 0; ptr_i < locations.end(); ptr_i++, i++) {
-        int item_i = *ptr_i;
         int sum = 0, count = 1;
         for (ptr_j = locations.begin() + i; ptr_j < locations.end() && *ptr_j <= *ptr_i + _params.consensus_interval; ptr_j++) {
             sum += (*ptr_j) - (*ptr_i);
@@ -47,7 +86,7 @@ int consensus(std::vector<int> locations, int imprecise_pos, params &_params) {
 
 // MARK: REFINEMENT
 
-int refine_start(int chrom, int outer_start, int inner_start, int imprecise_pos, params &_params) {
+void refine_start(int chrom, int outer_start, int inner_start, int imprecise_pos, result &res, params &_params, svtype type=DELETION) {
 
     // sam_read1 variables
     int32_t pos;
@@ -63,35 +102,44 @@ int refine_start(int chrom, int outer_start, int inner_start, int imprecise_pos,
     
     if (iter) {
         while (sam_itr_next( _params.htslib.fp_in, iter, aln ) > 0) {
+
             pos = aln->core.pos;
             uint32_t *cigar = bam_get_cigar(aln);
             
             // If read ends in between (outer_start, inner_start)
-            if ( bam_cigar_op( cigar[aln->core.n_cigar-1] ) == __CIGAR_SOFT_CLIP) {
-                int reference_pos = pos;
-                for ( int i = 0; i < aln->core.n_cigar; i++ ) {
-                    if ( bam_cigar_op( cigar[i] ) != __CIGAR_INSERTION && bam_cigar_op( cigar[i] ) != __CIGAR_SOFT_CLIP && bam_cigar_op( cigar[i] ) != __CIGAR_HARD_CLIP && bam_cigar_op( cigar[i] ) != __CIGAR_PADDING ) {
-                        reference_pos += bam_cigar_oplen( cigar[i] );
-                    }
-                }
-                
-                if ( outer_start <= reference_pos && reference_pos <= inner_start ) {
+            int reference_pos = pos;
+            for ( int i = 0; i < aln->core.n_cigar; i++ ) {
+                // If deletion is large enough to be considered as sv
+                // and it is covered in cigar string as the read is long enough.
+                if ( outer_start <= reference_pos && reference_pos <= inner_start && bam_cigar_op( cigar[i]) == __CIGAR_DELETION && bam_cigar_oplen( cigar[i]) >= __SV_MIN_LENGTH ) {
                     start_positions.push_back(reference_pos+1);
                 }
+
+                if ( bam_cigar_op( cigar[i] ) != __CIGAR_INSERTION && bam_cigar_op( cigar[i] ) != __CIGAR_SOFT_CLIP && bam_cigar_op( cigar[i] ) != __CIGAR_HARD_CLIP && bam_cigar_op( cigar[i] ) != __CIGAR_PADDING ) {
+                    reference_pos += bam_cigar_oplen( cigar[i] );
+                }
             }
+            
+            // Refinement base on Breakpoints
+            if ( type == DELETION && bam_cigar_op( cigar[aln->core.n_cigar-1] ) == __CIGAR_SOFT_CLIP && outer_start <= reference_pos && reference_pos <= inner_start ) {
+                start_positions.push_back(reference_pos+1);
+            }
+
+            if ( inner_start < reference_pos )
+                    break;
         }
     } else {
-        std::cout << "# invalid interval, iter is null. pos: " << imprecise_pos << std::endl;
+        _params.verbose && std::cout << " Invalid interval, iter is null. pos: " << imprecise_pos << std::endl;
     }
 
     bam_destroy1(aln);
     sam_itr_destroy(iter);
 
-    return consensus(start_positions, imprecise_pos, _params);
+    res.refined_start = consensus_pos(start_positions, imprecise_pos, _params);
 };
 
 
-int refine_end(int chrom, int inner_end, int outer_end, int imprecise_pos, params &_params) {
+void refine_end(int chrom, int inner_end, int outer_end, int imprecise_pos, result &res, params &_params, svtype type=DELETION) {
     
     // sam_read1 variables
     int32_t pos;
@@ -114,19 +162,35 @@ int refine_end(int chrom, int inner_end, int outer_end, int imprecise_pos, param
             if ( bam_cigar_op( cigar[0] ) == __CIGAR_SOFT_CLIP && inner_end <= pos && pos <= outer_end  ) {
                 end_positions.push_back(pos+1);
             }
+
+            int reference_pos = pos;
+            for ( int i = 0; i < aln->core.n_cigar; i++ ) {
+                if ( bam_cigar_op( cigar[i] ) != __CIGAR_INSERTION && bam_cigar_op( cigar[i] ) != __CIGAR_SOFT_CLIP && bam_cigar_op( cigar[i] ) != __CIGAR_HARD_CLIP && bam_cigar_op( cigar[i] ) != __CIGAR_PADDING ) {
+                    reference_pos += bam_cigar_oplen( cigar[i] );
+                }
+
+                // If deletion is large enough to be considered as sv
+                // and it is covered in cigar string as the read is long enough.
+                if ( type == DELETION && inner_end <= reference_pos && reference_pos <= outer_end && bam_cigar_op( cigar[i]) == __CIGAR_DELETION && bam_cigar_oplen( cigar[i]) >= __SV_MIN_LENGTH ) {
+                    end_positions.push_back(reference_pos+1);
+                }
+
+                if ( outer_end < reference_pos )
+                    break;
+            }
         }
     } else {
-        std::cout << "# invalid interval, iter is null. pos: " << imprecise_pos << std::endl;
+        _params.verbose && std::cout << "# invalid interval, iter is null. pos: " << imprecise_pos << std::endl;
     }
     
     bam_destroy1(aln);
     sam_itr_destroy(iter);
     
-    return consensus(end_positions, imprecise_pos, _params);
+    res.refined_end = consensus_pos(end_positions, imprecise_pos, _params);
 };
 
 
-int refine_point(int chrom, int start, int end, int imprecise_pos, params &_params) {
+void refine_point(int chrom, int start, int end, int imprecise_pos, result &res, params &_params, svtype type=INSERTION) {
     
     // sam_read1 variables
     int32_t pos;
@@ -139,37 +203,45 @@ int refine_point(int chrom, int start, int end, int imprecise_pos, params &_para
 
     iter = sam_itr_queryi( _params.htslib.bam_file_index, chrom - 1, start - _params.wider_interval, end + _params.narrow_interval);
     std::vector<int> positions;
+    std::vector<int> lengths;
     
     if (iter) {
         while (sam_itr_next( _params.htslib.fp_in, iter, aln ) > 0) {
             pos = aln->core.pos;
             uint32_t *cigar = bam_get_cigar(aln);
+
+            // If read starts in between (start, end)
+            if ( bam_cigar_op( cigar[0] ) == __CIGAR_SOFT_CLIP && start <= pos && pos <= end  ) {
+                positions.push_back(pos+1);
+            }
             
             // If read ends in between (start, end)
-            if ( bam_cigar_op( cigar[aln->core.n_cigar-1] ) == __CIGAR_SOFT_CLIP) {
-                int reference_pos = pos;
-                for ( int i = 0; i < aln->core.n_cigar; i++ ) {
-                    if ( bam_cigar_op( cigar[i] ) != __CIGAR_INSERTION && bam_cigar_op( cigar[i] ) != __CIGAR_SOFT_CLIP && bam_cigar_op( cigar[i] ) != __CIGAR_HARD_CLIP && bam_cigar_op( cigar[i] ) != __CIGAR_PADDING ) {
-                        reference_pos += bam_cigar_oplen( cigar[i] );
-                    }
+            int reference_pos = pos;
+            for ( int i = 0; i < aln->core.n_cigar; i++ ) {
+                if ( bam_cigar_op( cigar[i] ) != __CIGAR_INSERTION && bam_cigar_op( cigar[i] ) != __CIGAR_SOFT_CLIP && bam_cigar_op( cigar[i] ) != __CIGAR_HARD_CLIP && bam_cigar_op( cigar[i] ) != __CIGAR_PADDING ) {
+                    reference_pos += bam_cigar_oplen( cigar[i] );
                 }
-                if ( start <= reference_pos && reference_pos <= end ) {
+
+                if ( type == INSERTION && start <= reference_pos && reference_pos <= end && bam_cigar_op( cigar[i] ) == __CIGAR_INSERTION && bam_cigar_oplen( cigar[i] ) > __SV_MIN_LENGTH ) {
                     positions.push_back(reference_pos+1);
+                    lengths.push_back( bam_cigar_oplen( cigar[i] ) );
                 }
             }
-            // If read starts in between (start, end)
-            if ( bam_cigar_op( cigar[0] ) == __CIGAR_SOFT_CLIP && start <= pos && pos <= end ) {
-                positions.push_back(pos);
+            if ( start <= reference_pos && reference_pos <= end ) {
+                positions.push_back(reference_pos+1);
             }
         }
     } else {
-        std::cout << "# invalid interval, iter is null. pos: " << imprecise_pos << std::endl;
+        _params.verbose && std::cout << "# invalid interval, iter is null. pos: " << imprecise_pos << std::endl;
     }
 
     bam_destroy1(aln);
     sam_itr_destroy(iter);
     
-    return consensus(positions, imprecise_pos, _params);
+    res.refined_start = consensus_pos(positions, imprecise_pos, _params);
+    if ( type == INSERTION ) {
+        res.svlen = consensus(lengths, _params);
+    }
 };
 
 #endif
