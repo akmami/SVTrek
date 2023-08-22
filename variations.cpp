49b2f128b14f52ae540896060a86eb94faa405eb
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2023-08-22 13:24:54 +0300

    filter for sv refinement is implemented

--- variations.cpp
+++ variations.cpp
@@ -8,29 +8,43 @@
 namespace sv {
 
 	result deletion(int chrom, int outer_start, int inner_start, int inner_end, int outer_end, int imprecise_pos, int imprecise_end, params &_params) {
-	    
+
 		result res;
-	    refine_start(chrom, outer_start, inner_start, imprecise_pos, res, _params, DELETION);
-	    refine_end(chrom, inner_end, outer_end, imprecise_end, res, _params, DELETION);
-	    
-	    return res;
+		res.refined_start = -1;
+		res.refined_end = -1;
+		if ( ( imprecise_end - imprecise_pos ) < __CI_MIN_FILTER_LENGTH || ( imprecise_end - imprecise_pos ) * _params.ci_max_length > inner_start - outer_start ) {
+			refine_start(chrom, outer_start, inner_start, imprecise_pos, res, _params, DELETION);
+		}
+		if ( ( imprecise_end - imprecise_pos ) < __CI_MIN_FILTER_LENGTH || ( imprecise_end - imprecise_pos ) * _params.ci_max_length > outer_end - inner_end ) {
+			refine_end(chrom, inner_end, outer_end, imprecise_end, res, _params, DELETION);
+		}
+
+		return res;
 	};
 
 	result insertion(int chrom, int outer_start, int inner_start, int imprecise_pos,  params &_params) {
 
 		result res;
-	    refine_point(chrom, outer_start, inner_start, imprecise_pos, res, _params, INSERTION);
+		res.refined_start = -1;
+		res.svlen = -1;
+		refine_point(chrom, outer_start, inner_start, imprecise_pos, res, _params, INSERTION);
 
-	    return res;
+		return res;
 	};
 
 	result inversion(int chrom, int outer_start, int inner_start, int inner_end, int outer_end, int imprecise_pos, int imprecise_end, params &_params) {
 
-	    result res;
-	   	refine_point(chrom, outer_start, inner_start, imprecise_pos, res, _params);
-	    refine_point(chrom, inner_end, outer_end, imprecise_end, res, _params);
+		result res;
+		res.refined_start = -1;
+		res.refined_end = -1;
+		if ( ( imprecise_end - imprecise_pos ) < __CI_MIN_FILTER_LENGTH || ( imprecise_end - imprecise_pos ) * _params.ci_max_length > inner_start - outer_start ) {
+			refine_point(chrom, outer_start, inner_start, imprecise_pos, res, _params);
+		}
+		if ( ( imprecise_end - imprecise_pos ) < __CI_MIN_FILTER_LENGTH || ( imprecise_end - imprecise_pos ) * _params.ci_max_length > outer_end - inner_end ) {
+			refine_point(chrom, inner_end, outer_end, imprecise_end, res, _params);
+		}
 
-	    return res;
+		return res;
 	};
 
 };
